400fbe62000899d9b9ba748f4b0cde1afd18d3d0
tayc-007 <tayseersyed1@hotmail.com>
2023-03-27 00:22:44 +0100

    font

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,16 +1,7 @@
 import { extendTheme } from '@chakra-ui/react'
 
 const theme = extendTheme({
-  color:{
-    primary:` background-color: #67009a;
-    background-image: linear-gradient(to right, #67009a, #244cbe);
-    background-size: 100%;
-    -webkit-background-clip: text;
-    -moz-background-clip: text;
-    -webkit-text-fill-color: transparent;
-    -moz-text-fill-color: transparent;
-    `
-  },
+
   fonts: {
     heading: `'Montserrat', sans-serif`,
     body: `'Montserrat', sans-serif`,
