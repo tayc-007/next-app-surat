084bb426f49d0a483c44222bbf664454592cd6b7
tayc-007 <tayseersyed1@hotmail.com>
2023-04-08 01:22:22 +0100

    pixel-verify-domain

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta name="facebook-domain-verification" content="3we1l5psaoel1yyip2nxpn1ozoo892" />
       <noscript><img height="1" width="1" style={{display:"none"}}
       src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
       /></noscript>
