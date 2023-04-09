b55a37766eb1de848132c7d3c0241d679ee4e4c8
tayc-007 <tayseersyed1@hotmail.com>
2023-04-09 01:09:33 +0100

    gtm

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,10 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import { FB_PIXEL_ID } from './lib/fpixel'
+
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="facebook-domain-verification" content="3we1l5psaoel1yyip2nxpn1ozoo892" />
-      <noscript><img height="1" width="1" style={{display:"none"}}
-      src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
-      /></noscript>
+      
       </Head>
       <body>
         <Main />
--- pages/components/LandingPage.tsx
+++ pages/components/LandingPage.tsx
@@ -2,11 +2,9 @@ import Image from "next/image";
 import {Link} from "react-scroll";
 import { ChakraProvider,Hide,Center,GridItem,Grid,Button, Card, CardHeader, CardBody, CardFooter,Text,Heading,Stack,StackDivider,Box ,SimpleGrid  ,Container} from '@chakra-ui/react'
 import styles from "@/styles/stats.module.css";
-import * as fbq from "../lib/fpixel"
+
 export default function LandingPage(){
-  const handleClick = () =>{
-    fbq.event('track','Lead')
-  }
+
 return(
  
 <>
@@ -41,7 +39,7 @@ return(
       bgGradient="linear-gradient(to right, #67009a, #244cbe)" 
       color="white" 
       borderRadius="20px">
-    <Link onClick={()=> handleClick()} to="call" spy={true} smooth={true} offset={50} duration={500}>Book Your Call Now</Link>
+    <Link to="call" spy={true} smooth={true} offset={50} duration={500}>Book Your Call Now</Link>
     </Button>
   </Container>
   </GridItem>
