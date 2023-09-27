389c8b2d788ece75344b250a9192bb044b668b0e
tayc-007 <tayseersyed1@hotmail.com>
2023-09-27 23:31:04 +0100

    fix 2

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,11 @@
 // pages/_app.js
-import {useEffect} from 'react'
-import { useRouter } from 'next/router'
+
 
 import theme from '@/styles/theme';
 import { ChakraProvider } from '@chakra-ui/react'
 
 
-function MyApp({ Component, pageProps }) {
-  const router = useRouter()
 
-  useEffect(() => {
-    import('react-facebook-pixel')
-      .then((x) => x.default)
-      .then((ReactPixel) => {
-        ReactPixel.init('1053056662317635')
-        ReactPixel.pageView()
-
-        router.events.on('routeChangeComplete', () => {
-          ReactPixel.pageView()
-        })
-      })
-  }, [router.events])
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
