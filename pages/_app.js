5b611ba8850c40aab7a99046729036cf870c7bf0
tayc-007 <tayseersyed1@hotmail.com>
2023-09-29 00:57:06 +0100

    fix 50

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,29 @@
 // pages/_app.js
 
 
-import theme from '@/styles/theme';
+// import theme from '@/styles/theme';
+// import { ChakraProvider } from '@chakra-ui/react'
+
+
+
+//   return (
+//     <ChakraProvider theme={theme}>
+//       <Component {...pageProps} />
+//     </ChakraProvider>   
+//   )
+
+
+// export default MyApp
+import '@/styles/globals.css'
+
+// pages/_app.js
 import { ChakraProvider } from '@chakra-ui/react'
-
-
-
+import theme from '@/styles/theme.ts'
+function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
-    </ChakraProvider>   
+    </ChakraProvider>
   )
 }
 
